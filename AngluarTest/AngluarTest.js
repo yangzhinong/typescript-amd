0f52b7f6cc68c156f8a82d70245376915fefcd30
yzn <66845701@qq.com>
2016-09-07 00:49:21 +0800

    增加名称空间

--- AngluarTest/AngluarTest.js
+++ AngluarTest/AngluarTest.js
@@ -1,10 +1,23 @@
 var myApp = angular.module("myApp", [])
-    .controller("myController", function ($scope) {
+    .filter("gender", function () {
+    return function (gender) {
+        switch (gender) {
+            case 1:
+                return "Male";
+            case 2:
+                return "Female";
+            case 3:
+                return "Not disclosed";
+        }
+    };
+})
+    .controller("myController", function ($scope, $http) {
     var tests = [
         { name: 'yzn', likes: 5, dislikes: 0 },
         { name: 'yq', likes: 0, dislikes: 0 },
         { name: 'rr', likes: 0, dislikes: 0 }
     ];
+    var xScrop = $scope;
     $scope.tests = tests;
     $scope.addLike = function (t) {
         t.likes++;
@@ -12,5 +25,7 @@ var myApp = angular.module("myApp", [])
     $scope.decLike = function (t) {
         t.dislikes++;
     };
+    $http.post('').then(function (data) { });
+    $http({ method: 'GET', url: '' }).then(function () { });
 });
 //# sourceMappingURL=AngluarTest.js.map
--- AngluarTest/AngluarTest.ts
+++ AngluarTest/AngluarTest.ts
@@ -1,20 +1,46 @@
-﻿    var myApp = angular.module("myApp", [])
-        .controller("myController", function ($scope) {
-            interface Test {
-                name: string;
-                likes: number;
-                dislikes: number
+﻿declare namespace angularTest {
+    interface Test {
+        name: string;
+        likes: number;
+        dislikes: number
+    }
+
+}
+
+var myApp = angular.module("myApp", [])
+    .filter("gender", function () {
+        return function (gender:number) {
+            switch (gender) {
+                case 1:
+                    return "Male";
+                    
+                case 2:
+                    return "Female";
+                case 3:
+                    return "Not disclosed";
             }
-            var tests: Test[] = [
+        }
+    })
+        .controller("myController", function ($scope,$http:angular.IHttpService) {
+            
+            var tests: angularTest.Test[] = [
                 { name: 'yzn', likes: 5, dislikes: 0 },
                 { name: 'yq', likes: 0, dislikes: 0 },
                 { name: 'rr', likes: 0, dislikes: 0 }
             ];
+            var xScrop = <angular.IScope>$scope;
+            
             $scope.tests = tests;
-            $scope.addLike = function (t:Test) {
+            $scope.addLike = function (t:angularTest.Test) {
                 t.likes++;
             };
-            $scope.decLike = function (t:Test) {
+            $scope.decLike = function (t:angularTest.Test) {
                 t.dislikes++;
             };
+            $http.post('').then(function (data: IJsonMsg) { });
+            
+            $http({method:'GET',url:''}).then(function () { });
+
+
+            
         });
